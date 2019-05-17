b697186b88a2179f6f4a02b11ce9e5d6591993f7
TRUECALLER <truecaller@TRUECALLERs-MacBook-Air-3.local>
2019-05-17 19:13:58 +0530

    Added class

--- weight_sort.rb
+++ weight_sort.rb
@@ -1,41 +1,55 @@
-def sum_of_digits(num)
-  sum = 0
-  while( num > 0 )
-    digit =num % 10
-    sum += r
-    num /= 10
+class String_int_array
+  attr_accessor :input 
+  
+  def initialize(input)
+    @input=input
   end
-  return sum
-end
-
-
-input = gets()
-input = input.split()
-input = input.map(&:to_i)
-
-inp_sorted_by_sum = input.sort_by { |int| sum_of_digits(int) }
-
-output = []
-i = 0
-
-while( i < input.length )
   
-  temp = [inp_sorted_by_sum[i]]
-  
-  while( i < input.length-1 && sum_of_digits( inp_sorted_by_sum[i]) == sum_of_digits( inp_sorted_by_sum[i+1] ))
+  def sum_of_digits(num)
+    sum = 0
     
-    temp << inp_sorted_by_sum[i+1]
-    i += 1
+    while( num > 0 )
+      digit = num % 10
+      sum += digit
+      num /= 10
+    end
+    
+    return sum
+  end
+
+  def weight_sort
+    @input = @input.map(&:to_i)
+    @input = @input.sort_by{ |int| sum_of_digits(int) }
+	
+    i = 0
+    output = []
+	
+    while(i < @input.length)
+      temp = [@input[i]]
+      
+      while(i < @input.length-1 && sum_of_digits(@input[i]) == sum_of_digits(@input[i+1]))
+        temp << @input[i+1]
+        i += 1
+      end
+  
+      temp = temp.sort_by{|x| x.to_s }
+  
+      for x in temp
+        output << x
+      end
+  
+      i += 1
+   end
+   
+   @input = output.map(&:to_s)
   
   end
-  
-  temp = temp.sort_by{|x| x.to_s }
-  
-  for x in temp
-    output << x
-  end
-  
-  i += 1	
 end
 
-print( output , "\n" )
+
+str = gets()
+ob1 = String_int_array.new(str.split())
+
+ob1.weight_sort
+
+print(ob1.input)
